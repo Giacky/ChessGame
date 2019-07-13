d6b4251028855f71631e90c14ecccbf87bec32c5
Giacky <giacomo.anerdi@gmail.com>
2019-07-14 01:16:43 +0200

    Made horizontal, vertical and diagonal moves code more compact

--- src/board/Board.java
+++ src/board/Board.java
@@ -20,19 +20,48 @@ public class Board {
 
     public ArrayList<Move> piecePossibleMoves(Piece piece) {
         ArrayList<Move> possibleMoves = new ArrayList<>();
+        Point position = piece.getPosition();
         switch (piece.getType()) {
             case QUEEN:
+                possibleMoves.addAll(hvdMoves(piece, true, true));
                 break;
             case KING:
+                for (int dx=-1; dx<2; dx++) {
+                    for (int dy=-1; dy<2; dy++) {
+                        if (dx!=0 && dy!=0) {
+                            Point newPosition = new Point(position);
+                            newPosition.moveBy(dx, dy);
+                            if (newPosition.x >= 0 && newPosition.y >= 0 && newPosition.x < 8 && newPosition.y < 8 && board[newPosition.x][newPosition.y] == null) {
+                                possibleMoves.add(new Move(position, newPosition));
+                            }
+                        }
+                    }
+                }
                 break;
             case KNIGHT:
+                for (int i=-3; i<4; i+=6) {
+                    for (int j = -2; j < 3; j+=4) {
+                        Point newPosition = new Point(position);
+                        newPosition.moveBy(i, j);
+                        if (newPosition.x >= 0 && newPosition.y >= 0 && newPosition.x < 8 && newPosition.y < 8 && board[newPosition.x][newPosition.y] == null) {
+                            possibleMoves.add(new Move(position, newPosition));
+                        }
+                        newPosition = new Point(position);
+                        newPosition.moveBy(j, i);
+                        if (newPosition.x >= 0 && newPosition.y >= 0 && newPosition.x < 8 && newPosition.y < 8 && board[newPosition.x][newPosition.y] == null) {
+                            possibleMoves.add(new Move(position, newPosition));
+                        }
+                    }
+                }
                 break;
             case BISHOP:
+                possibleMoves.addAll(hvdMoves(piece, false, true));
                 break;
             case ROOK:
-
+                possibleMoves.addAll(hvdMoves(piece, true, false));
                 break;
             case PAWN:
+                
                 break;
         }
         return possibleMoves;
@@ -42,56 +71,31 @@ public class Board {
 
     }
 
-    private ArrayList<Move> diagonalMoves() {
-
-    }
-
-    private ArrayList<Move> horizontalVerticalMoves(Point position) {
+    private ArrayList<Move> hvdMoves(Piece piece, boolean hvMoves, boolean dMoves) {
+        Point position = piece.getPosition();
         ArrayList<Move> possibleMoves = new ArrayList<>();
-
-        int d = 1;
-
-        //Forward
-        while(true) {
-            Point newPosition = new Point(position);
-            newPosition.moveBy(0, d);
-            if (newPosition.y < 8 && board[newPosition.x][newPosition.y] != null) {
-                possibleMoves.add(new Move(position, newPosition));
-            } else {
-                break;
-            }
-        }
-
-        //Right
-        while(true) {
-            Point newPosition = new Point(position);
-            newPosition.moveBy(d, 0);
-            if (newPosition.x < 8 && board[newPosition.x][newPosition.y] != null) {
-                possibleMoves.add(new Move(position, newPosition));
-            } else {
-                break;
-            }
-        }
-
-        //Backwards
-        while(true) {
-            Point newPosition = new Point(position);
-            newPosition.moveBy(0, -d);
-            if (newPosition.y >= 0 && board[newPosition.x][newPosition.y] != null) {
-                possibleMoves.add(new Move(position, newPosition));
-            } else {
-                break;
-            }
-        }
-
-        //Left
-        while(true) {
-            Point newPosition = new Point(position);
-            newPosition.moveBy(-d, 0);
-            if (newPosition.x >= 0  && board[newPosition.x][newPosition.y] != null) {
-                possibleMoves.add(new Move(position, newPosition));
-            } else {
-                break;
+        for (int dx=-1; dx<2; dx++) {
+            for (int dy=-1; dy<2; dy++) {
+                if ( (hvMoves &&(dx == 0 || dy == 0) && !(dx == 0 && dy == 0)) || (dMoves && !(dx == 0 || dy == 0)) ) {
+                    int m = 1;
+                    while(true) {
+                        Point newPosition = new Point(position);
+                        newPosition.moveBy(dx * m, dy * m);
+                        if (newPosition.x >= 0) {
+                            if (board[newPosition.x][newPosition.y] == null) {
+                                possibleMoves.add(new Move(position, newPosition));
+                            } else if (board[newPosition.x][newPosition.y].getPlayer() != piece.getPlayer()) {
+                                possibleMoves.add(new Move(position, newPosition));
+                                break;
+                            } else {
+                                break;
+                            }
+                        } else {
+                            break;
+                        }
+                        m++;
+                    }
+                }
             }
         }
         return possibleMoves;
