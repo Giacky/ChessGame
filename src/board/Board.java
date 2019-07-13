c6a95ec3a53521bc2c2c78e065c061ad1591df2f
Giacky <giacomo.anerdi@gmail.com>
2019-07-13 21:59:06 +0200

    Started making the possible moves logic for the pieces\

--- src/board/Board.java
+++ src/board/Board.java
@@ -2,11 +2,12 @@ package board;
 
 import utility.Move;
 import utility.Player;
+import utility.Point;
 
 import java.util.ArrayList;
 
 public class Board {
-    private Tile[][] board;
+    private Piece[][] board;
     private Player player;
 
     public Board() {
@@ -18,13 +19,82 @@ public class Board {
     }
 
     public ArrayList<Move> piecePossibleMoves(Piece piece) {
-        switch (piece) {
-            case
+        ArrayList<Move> possibleMoves = new ArrayList<>();
+        switch (piece.getType()) {
+            case QUEEN:
+                break;
+            case KING:
+                break;
+            case KNIGHT:
+                break;
+            case BISHOP:
+                break;
+            case ROOK:
+
+                break;
+            case PAWN:
+                break;
         }
+        return possibleMoves;
     }
 
     public ArrayList<Move> allPossibleMoves() {
 
+    }
+
+    private ArrayList<Move> diagonalMoves() {
+
+    }
+
+    private ArrayList<Move> horizontalVerticalMoves(Point position) {
+        ArrayList<Move> possibleMoves = new ArrayList<>();
+
+        int d = 1;
+
+        //Forward
+        while(true) {
+            Point newPosition = new Point(position);
+            newPosition.moveBy(0, d);
+            if (newPosition.y < 8 && board[newPosition.x][newPosition.y] != null) {
+                possibleMoves.add(new Move(position, newPosition));
+            } else {
+                break;
+            }
+        }
+
+        //Right
+        while(true) {
+            Point newPosition = new Point(position);
+            newPosition.moveBy(d, 0);
+            if (newPosition.x < 8 && board[newPosition.x][newPosition.y] != null) {
+                possibleMoves.add(new Move(position, newPosition));
+            } else {
+                break;
+            }
+        }
+
+        //Backwards
+        while(true) {
+            Point newPosition = new Point(position);
+            newPosition.moveBy(0, -d);
+            if (newPosition.y >= 0 && board[newPosition.x][newPosition.y] != null) {
+                possibleMoves.add(new Move(position, newPosition));
+            } else {
+                break;
+            }
+        }
+
+        //Left
+        while(true) {
+            Point newPosition = new Point(position);
+            newPosition.moveBy(-d, 0);
+            if (newPosition.x >= 0  && board[newPosition.x][newPosition.y] != null) {
+                possibleMoves.add(new Move(position, newPosition));
+            } else {
+                break;
+            }
+        }
+        return possibleMoves;
     }
 
 }
--- src/board/Piece.java
+++ src/board/Piece.java
@@ -2,6 +2,7 @@ package board;
 
 import utility.Move;
 import utility.Player;
+import utility.Point;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -10,12 +11,43 @@ public abstract class Piece {
     private Player player;
     private Image texture;
     private PieceType type;
+    private Point position; //(0, 0) bottom left
 
-    public Piece(Player player, PieceType type) {
+    public Piece(Player player, Point position, PieceType type) {
         this.player = player;
         this.type = type;
+        this.position = position;
     }
 
+    public PieceType getType() {
+        return type;
+    }
 
+    public Player getPlayer() {
+        return player;
+    }
 
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+
+    public Image getTexture() {
+        return texture;
+    }
+
+    public void setTexture(Image texture) {
+        this.texture = texture;
+    }
+
+    public void setType(PieceType type) {
+        this.type = type;
+    }
 }
